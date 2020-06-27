109b51710116c60cc2131db87a9ab33b2ade02fc
niaki <nina.zizakic@ugent.be>
2020-06-27 22:21:48 +0200

    Add additional info about the groceries (storage_time and ninz_friendly)

--- lib.py
+++ lib.py
@@ -1,8 +1,17 @@
 class Grocery:
-    def __init__(self, name, unit, long_lasting=False, ninz_friendly=False):
+    def __init__(self, name, unit, storage_time=1, ninz_friendly=False):
         self.name = name
         self.unit = unit
-        self.ninz_friendly = ninz_friendly
+        self.storage_time = storage_time  # in weeks, rounded down
+        self.ninz_friendly = ninz_friendly  # how difficult it is for me to buy
+
+# Legend for ninz_friendly
+# 0   -- have no idea how to assess if it’s good or not
+# 0.2 -- not very good at assessing if it’s good or not
+# 0.4 -- it’s a hard fruit/vegetable, so probably can’t screw up much
+# 0.6 -- it’s a packaged product, but not sure maybe which one
+# 0.8 -- it’s a packaged product, and I know exactly which one
+# 1   -- am buying it for myself and know exactly what I want
 
 
 class RecipeIngredient:
